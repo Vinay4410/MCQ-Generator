9f0b88e491b533cdd3119470d4de197ce55c96c1
vinay4410 <vinaypatel4476@gmail.com>
2024-01-03 16:18:29 +0530

    Second Commit

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-import pandas as pd
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='vinay',
+    author_email='vinay@upforce.tech',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
